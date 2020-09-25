80b4cd96df01bfbb77c8a8d7ae35bbbef242c6ee
surikbs <surikbs@gmail.com>
2020-09-25 13:27:17 +0530

    update

--- HandsonExercises/HandsonExercise1.go
+++ HandsonExercises/HandsonExercise1.go
@@ -1,21 +1,45 @@
+// package main
+
+// import "fmt"
+
+// var x int = 42
+// var y string = "James Bond"
+// var z bool = true
+
+// func main() {
+// x := 42
+// y := "James Bond"
+// z := true
+// fmt.Println(x, y, z)
+// fmt.Println(x)
+// fmt.Println(y)
+// fmt.Println(z)
+
+// s := fmt.Sprintf("%v \t %v \t %v ", x, y, z)
+
+// fmt.Println(s)
+// }
+
+// Exercise go 4 and 5
+
 package main
 
 import "fmt"
 
-var x int = 42
-var y string = "James Bond"
-var z bool = true
+type hotdog int
+
+var x hotdog
+var y int
 
 func main() {
-	// x := 42
-	// y := "James Bond"
-	// z := true
-	// fmt.Println(x, y, z)
-	// fmt.Println(x)
-	// fmt.Println(y)
-	// fmt.Println(z)
+	fmt.Println(x)
+	fmt.Printf("%T\n", x)
+	x = 42
+	fmt.Println(x)
 
-	s := fmt.Sprintf("%v \t %v \t %v ", x, y, z)
+	//exercise 5
+	y = int(x)
+	fmt.Println(y)
+	fmt.Printf("%T", y)
 
-	fmt.Println(s)
 }
